056f6d286a47c7a6d43876ffca3fbe900b93ebd8
Igor <aligol6573@gmail.com>
2026-09-21 13:04:15 +0300

    test-now

---
+++ test_now.py
@@ -0,0 +1,16 @@
+import pytest
+from калькулятор_дробей import add, sub, mult, div
+
+
+# def test_add():
+#     assert add(1, 2, 1, 2) == (4, 4)
+#     assert add(1, 5, 2, 5) == (15, 25)
+
+@pytest.mark.parametrize('n1, d1, n2, d2, res',
+                         [
+                             (1, 2, 1, 2, (4, 4)),
+                             (1, 5, 2, 5, (15, 25))
+                         ])
+def test_add(n1, d1, n2, d2, res):
+    assert add(n1, d1, n2, d2 ) == res
+
